8f7abd2b31d7187d98f38885a85f578977c3cb16
Snycode <snydersvega147@gmail.com>
2021-07-07 20:40:04 -0500

    Creacion de la ventana inicio

--- IMD/src/app/app.module.ts
+++ IMD/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { BannerComponent } from './component/banner/banner.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
