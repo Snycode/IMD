24a0c763134ad303af6ea578a54d76d8a03a95ff
Snycode <snydersvega147@gmail.com>
2021-07-09 07:38:52 -0500

    Se agregaron los estilos y nuevas ventanas

--- IMD/src/app/app-routing.module.ts
+++ IMD/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BannerComponent } from './component/banner/banner.component';
+import { AboutComponent } from './component/about/about.component';
+import { ProductsComponent } from './component/products/products.component';
+import { ContactsComponent } from './component/contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: BannerComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: 'contacts', component: ContactsComponent},
+  { path: '**', component: BannerComponent },
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- IMD/src/app/app.module.ts
+++ IMD/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { BannerComponent } from './component/banner/banner.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { AboutComponent } from './component/about/about.component';
+import { ProductsComponent } from './component/products/products.component';
+import { ContactsComponent } from './component/contacts/contacts.component';
+
 
 
 
@@ -13,6 +18,11 @@ import { BannerComponent } from './component/banner/banner.component';
     AppComponent,
     HeaderComponent,
     BannerComponent,
+    FooterComponent,
+    AboutComponent,
+    ProductsComponent,
+    ContactsComponent,
+
   ],
   imports: [
     BrowserModule,
